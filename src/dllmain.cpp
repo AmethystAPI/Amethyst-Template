9b4fe62a37fdd9900aba9e2027f04ae18f6513c7
FrederoxDev <blakefreddie96@gmail.com>
2024-02-01 18:33:45 +0000

    Initial template for @1.2.0

---
+++ src/dllmain.cpp
@@ -0,0 +1,17 @@
+﻿#include "dllmain.h"
+
+// Ran when the mod is loaded into the game by AmethystRuntime
+ModFunction void Initialize(HookManager* hookManager, Amethyst::EventManager* eventManager, InputManager* inputManager) 
+{
+    // Logging from <Amethyst/Log.h>
+    Log::Info("Hello, Amethyst World!");
+
+    // Add a listener to a built in amethyst event
+    eventManager->onStartJoinGame.AddListener(OnStartJoinGame);
+}
+
+// Subscribed to amethysts on start join game event in Initialize
+void OnStartJoinGame(ClientInstance* client)
+{
+    Log::Info("The player has joined the game!");
+}
---
+++ src/dllmain.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Windows.h>
+#include <amethyst/events/EventManager.h>
+#include <amethyst/HookManager.h>
+#include <amethyst/InputManager.h>
+
+#define ModFunction extern "C" __declspec(dllexport)
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    return TRUE;
+}
+
+void OnStartJoinGame(ClientInstance* client);
